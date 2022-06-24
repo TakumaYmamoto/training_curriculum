7a8a376d88a935b1266289efce503a4a2fa7d855
TakumaYmamoto <104744174+TakumaYmamoto@users.noreply.github.com>
2022-06-24 22:14:50 +0900

    lssue2

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -12,7 +12,7 @@ class CalendarsController < ApplicationController
     redirect_to action: :index
   end
 
-  private
+  Private
 
   def plan_params
     params.require(:calendars).permit(:date, :plan)
@@ -34,7 +34,7 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month: (@todays_date + x).month, :date: (@todays_date+x).day, :plans: today_plans}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
 
